52756cbfeb73327dd0f985dcee46f0610f040111
greg <unixfan@yandex.ru>
2021-02-23 13:52:11 +0300

    - add fix for kafka consummer reader with offset storage

--- tasks/data_reader.py
+++ tasks/data_reader.py
@@ -15,6 +15,7 @@ def get_consumer(lg, data):
                              ssl_certfile=data.get('ssl_certfile_path'),
                              ssl_keyfile=data.get('ssl_keyfile_path'),
                              group_id=data.get('group_id'),
+                             auto_commit_interval_ms=100,
                              enable_auto_commit=True,
                              auto_offset_reset='latest',
                              value_deserializer=lambda v: json.loads(v).encode('utf-8'))
@@ -29,44 +30,42 @@ def transfer_message(lg):
     conf_data = get_config.get_monitor_data()
     group = conf_data.get('kafka').get('group_id')
     db_offset = db_utils.get_last_offset(group)
-    lg.info('DB stored offset is %s ', db_offset)
     consumer = get_consumer(lg, conf_data.get('kafka'))
     topic_partition = TopicPartition(conf_data.get('kafka').get('topic'), 0)
+    lg.info('topic partition value %s', topic_partition)
     consumer.assign([topic_partition])
-    # consumer.seek_to_end(topic_partition)
-    last_offset = consumer.position(topic_partition)
-    # consumer.seek_to_beginning(topic_partition)
-    consumer.seek(topic_partition, db_offset)
-
-    # consumer.seek_to_end(topic_partition)
-    # consumer.poll()
-    # consumer.seek_to_end()
+    last_offset = consumer.end_offsets([topic_partition])[topic_partition]
+    lg.info('last offset messages %s', last_offset)
+    if db_offset is None:
+        consumer.seek_to_beginning(topic_partition)
+    else:
+        consumer.seek(topic_partition, db_offset)
     try:
         for msg in consumer:
             lg.info("current message offset %s ", msg.offset)
-            if msg.offset == last_offset:
+            print('-- start getting messages ---')
+            lg.info('Message offset %s Last offset %s', msg.offset, last_offset)
+            bytes_msg = msg.value.replace(b"'", b'"')
+            obj_msg = Message.from_json(bytes_msg)
+            lg.info('Start inserting to DB')
+            lg.info(obj_msg)
+            db_utils.insert_rec(obj_msg)
+            lg.info('topic partition value %s', topic_partition)
+            if msg.offset >= last_offset -1:
                 #
-                lg.info('Finish reading last offset %s msg_offset %s ', last_offset, msg.offset)
+                lg.info('Finish reading last offset %s', last_offset)
                 db_utils.insert_offset(group, msg.offset)
-                lg.info('Update DB offset value %s ', msg.offset)
+                lg.info('msg offset value %s ', msg.offset)
                 break
-            else:
-                print('-- start getting messages ---')
-                lg.info('Message offset %s Last offset %s', msg.offset, last_offset)
-                bytes_msg = msg.value.replace(b"'", b'"')
-                obj_msg = Message.from_json(bytes_msg)
-                lg.info('Start inserting to DB')
-                lg.info(obj_msg)
-                db_utils.insert_rec(obj_msg)
-                lg.info('Insert to DB message offset %s', msg.offset)
-                db_utils.insert_offset(group, msg.offset)
-                lg.info('Finish inserting to DB')
+
     except Exception as ex:
+        consumer.commit()
+        consumer.close()
         lg.error(ex.args)
     finally:
         lg.info('Finish connection')
         consumer.commit()
-        consumer.close(5)
+        consumer.close()
 
 
 class MonitorDataReader:
